4643df1ba4138c0acfc1167b38f3c399dd0fe673
hamzakaya5 <kaya.hamza59@hotmail.com>
2025-04-28 18:28:25 +0300

    dockerfile prod and dockerfile dev docker compose

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func ServePage(writer http.ResponseWriter, reqest *http.Request) {
@@ -16,19 +18,32 @@ func ServePage(writer http.ResponseWriter, reqest *http.Request) {
 }
 
 func ServeAnother(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+	if r.Method != "GET" {
 		log.Fatal(http.ErrNotSupported)
 	}
 	io.WriteString(w, "Post request had been made")
 }
 
 func Actual() {
-	fileserver := http.FileServer(http.Dir("./myfiles"))
-	http.Handle("/", fileserver)
-	http.HandleFunc("/hello", ServePage)
-	http.HandleFunc("/content", ServeAnother)
-	if err := http.ListenAndServe(":8888", nil); err != nil {
+
+	mux := http.NewServeMux()
+	fileserver := http.FileServer(http.Dir("./backend/html-docs"))
+	mux.HandleFunc("/hello", ServePage)
+	mux.HandleFunc("/content", ServeAnother)
+	mux.Handle("/login", http.StripPrefix("/login", fileserver))
+
+	if err := http.ListenAndServe("0.0.0.0:8080", mux); err != nil {
 		log.Fatal("Server has been corrupted bro")
 	}
 
+}
+
+func GinServer() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
